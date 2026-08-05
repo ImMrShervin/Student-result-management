7f523649f35aa0acc43f59fcba416c04a4001fc3
Shervin Mirzaei <shervinmr84@gmail.com>
2026-08-05 16:40:04 +0330

    Update GradePublishedNotification.php

--- backend/app/Notifications/GradePublishedNotification.php
+++ backend/app/Notifications/GradePublishedNotification.php
@@ -6,7 +6,6 @@ namespace App\Notifications;
 
 
 
-use App\Models\Grade;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
